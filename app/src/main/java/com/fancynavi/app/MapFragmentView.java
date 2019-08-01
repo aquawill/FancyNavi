55b6cce74567c5d99b0bda5ce12ff0bcae773d1b
aquawill <aquawill@msn.com>
2019-08-01 14:32:31 +0800

    Minor Fix.

--- app/src/main/java/com/fancynavi/app/MapFragmentView.java
+++ app/src/main/java/com/fancynavi/app/MapFragmentView.java
@@ -685,7 +685,9 @@ class MapFragmentView {
                                     targetSignImageView.setVisibility(View.VISIBLE);
                                     if (!isSignShowing) {
                                         MediaPlayer mediaPlayer = MediaPlayer.create(m_activity, R.raw.beep_short);
-                                        mediaPlayer.start();
+                                        if (mediaPlayer != null) {
+                                            mediaPlayer.start();
+                                        }
                                     }
                                     isSignShowing = true;
                                     switch (trafficSignType) {
@@ -919,14 +921,15 @@ class MapFragmentView {
             if (searchResultSnackbar != null) {
                 searchResultSnackbar.dismiss();
             }
-            m_map.removeMapOverlay(selectedFeatureMapOverlay);
+            if (selectedFeatureMapOverlay != null) {
+                m_map.removeMapOverlay(selectedFeatureMapOverlay);
+            }
             new SearchResultHandler(m_activity.findViewById(R.id.mapFragmentView), pointF, m_map);
             return false;
         }
 
         @Override
         public boolean onDoubleTapEvent(PointF pointF) {
-            m_map.removeMapOverlay(selectedFeatureMapOverlay);
             touchToAddWaypoint(pointF);
             switchUiControls(View.VISIBLE);
             m_map.setCenter(pointF, Map.Animation.LINEAR, m_map.getZoomLevel(), m_map.getOrientation(), m_map.getTilt());
@@ -1127,7 +1130,9 @@ class MapFragmentView {
 
     private void touchToAddWaypoint(PointF p) {
         isDragged = true;
-        m_map.removeMapOverlay(selectedFeatureMapOverlay);
+        if (selectedFeatureMapOverlay != null) {
+            m_map.removeMapOverlay(selectedFeatureMapOverlay);
+        }
         GeoCoordinate touchPointGeoCoordinate = m_map.pixelToGeo(p);
         MapMarker mapMarker = new MapMarker(touchPointGeoCoordinate);
         mapMarker.setDraggable(true);
